4671cb259d5ca519e8a89c65a911216991348814
Ayaan-coder-stack <ayaan.programmer.2025.77@gmail.com>
2025-05-23 19:46:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1214 @@
+
+        // The list of 7-letter words from the provided file
+        const wordList = `NOTAIRE
+OTARINE
+ETAERIO
+ANEROID
+AILERON
+ALERION
+ALIENOR
+ELATION
+TOENAIL
+ERASION
+ATONIES
+OARIEST
+OTARIES
+ROASTIE
+ARENITE
+RETINAE
+TRAINEE
+ORIGANE
+GOATIER
+ANTIRED
+DETRAIN
+TRAINED
+ENTRAIL
+LATRINE
+RATLINE
+RELIANT
+RETINAL
+TRENAIL
+ANESTRI
+ANTSIER
+NASTIER
+RATINES
+RESIANT
+RETAINS
+RETINAS
+RETSINA
+STAINER
+STARNIE
+STEARIN
+RUINATE
+TAURINE
+URANITE
+URINATE
+DARIOLE
+ADONISE
+ANODISE
+SODAINE
+RADIOES
+ROADIES
+SOREDIA
+IODATES
+TOADIES
+ANISOLE
+SEALION
+ISOLATE
+INERTIA
+TORNADE
+ATONERS
+SANTERO
+SENATOR
+TREASON
+OUTEARN
+RETINOL
+NORITES
+OESTRIN
+ORIENTS
+STONIER
+TERSION
+TRIONES
+ROUTINE
+ALIENER
+LINEATE
+ATELIER
+REALTIE
+ETESIAN
+AERIEST
+SERIATE
+AERADIO
+AEOLIAN
+OLEARIA
+ENATION
+GRANITE
+GRATINE
+INGRATE
+TANGIER
+TEARING
+TAENIAE
+ARENOSE
+ROSEATE
+ORDINEE
+ELOINER
+TROELIE
+EROTISE
+NIOBATE
+ACONITE
+ANOETIC
+EROTICA
+MORAINE
+ROMAINE
+AMNIOTE
+GODETIA
+AGONIES
+AGONISE
+GOATIES
+NEGATOR
+GENITOR
+DILATER
+REDTAIL
+TRAILED
+TRIALED
+RANDIES
+SANDIER
+SARDINE
+UNAIRED
+URANIDE
+DESTAIN
+DETAINS
+INSTEAD
+NIDATES
+SAINTED
+SATINED
+STAINED
+AUDIENT
+ARIDEST
+ASTERID
+ASTRIDE
+DIASTER
+DISRATE
+STAIDER
+STAIRED
+TARDIES
+TIRADES
+TRADIES
+ALINERS
+NAILERS
+RENAILS
+EASTLIN
+ELASTIN
+ENTAILS
+NAILSET
+SALIENT
+SALTINE
+SLAINTE
+STANIEL
+TENAILS
+ALUNITE
+REALIST
+RETAILS
+SALTIER
+SALTIRE
+SLATIER
+TAILERS
+URALITE
+AUNTIES
+SINUATE
+REGINAE
+DIATRON
+AROINTS
+RATIONS
+RAINOUT
+ARANEID
+AIRDATE
+RADIATE
+TIARAED
+ANTLIAE
+ENTASIA
+TAENIAS
+ARISTAE
+ASTERIA
+ATRESIA
+DENARII
+INEDITA
+AIRLINE
+SENARII
+ISATINE
+AIRIEST
+IRISATE
+DEASOIL
+ISOLEAD
+DOULEIA
+EATERIE
+LADRONE
+TALONED
+DELATOR
+LEOTARD
+RONDEAU
+ASTONED
+DONATES
+ONSTEAD
+DOATERS
+ROASTED
+TORSADE
+TROADES
+OUTDARE
+OUTREAD
+READOUT
+LOANERS
+ORLEANS
+RELOANS
+ALEURON
+ETALONS
+TOLANES
+OESTRAL
+OLESTRA
+ROTULAE
+TORULAE
+ARENOUS
+SOUTANE
+LENTOID
+DOILTER
+DINEROS
+DONSIER
+INDORSE
+ORDINES
+ROSINED
+SORDINE
+DOURINE
+NEUROID
+OUNDIER
+DITONES
+STONIED
+EDITORS
+ROISTED
+ROSITED
+SORTIED
+STEROID
+STORIED
+TRIODES
+ETOURDI
+IODURET
+OUTRIDE
+LIENORS
+NEROLIS
+SONLIER
+ENTOILS
+LIONETS
+ONLIEST
+ELUTION
+OUTLINE
+ESTRIOL
+LOITERS
+TOILERS
+OUTLIER
+URINOSE
+OURIEST
+STOURIE
+TOURIES
+TOUSIER
+ENTRAIN
+TRANNIE
+RETRAIN
+TERRAIN
+TRAINER
+INTREAT
+ITERANT
+NATTIER
+NITRATE
+TARTINE
+TERTIAN
+ALIENED
+DELAINE
+LEADIER
+ANISEED
+DEARIES
+READIES
+IDEATES
+AUDITEE
+SEALINE
+EARLIES
+REALISE
+ALTERNE
+ENTERAL
+ETERNAL
+TELERAN
+EARNEST
+EASTERN
+NEAREST
+RATEENS
+STERANE
+ENTIRES
+ENTRIES
+NERITES
+RETINES
+TRENISE
+TRIENES
+NEURITE
+RETINUE
+REUNITE
+UTERINE
+ATEBRIN
+CANTIER
+CERATIN
+CERTAIN
+CREATIN
+CRINATE
+NACRITE
+TACRINE
+FAINTER
+FENITAR
+HAIRNET
+INEARTH
+THERIAN
+MERANTI
+MINARET
+RAIMENT
+INAPTER
+PAINTER
+PERTAIN
+REPAINT
+TAWNIER
+TINWARE
+AEROSAT
+EDITION
+TENIOID
+DIORITE
+NOILIER
+ETIOLIN
+IONISER
+IRONIES
+IRONISE
+NOISIER
+INOSITE
+RIOTISE
+OATIEST
+OSTIATE
+TOASTIE
+AIERIES
+AREDING
+DEARING
+DERAIGN
+EARDING
+GRADINE
+GRAINED
+READING
+TRIAGED
+ALIGNER
+ENGRAIL
+LAERING
+LEARING
+NARGILE
+REALIGN
+REGINAL
+ATINGLE
+ELATING
+GELATIN
+GENITAL
+TAGLINE
+ANGRIES
+EARINGS
+ERASING
+GAINERS
+GRAINES
+REAGINS
+REGAINS
+REGINAS
+SEARING
+SERINGA
+EASTING
+EATINGS
+GAINEST
+GENISTA
+INGATES
+INGESTA
+SEATING
+TAGINES
+TANGIES
+TEASING
+TSIGANE
+UNITAGE
+AGISTER
+AIGRETS
+GAITERS
+SEAGIRT
+STAGIER
+STRIGAE
+TIRAGES
+TRIAGES
+ORATION
+NORTENA
+ORNATER
+INTONER
+NOINTER
+TERNION
+TRITONE
+OREADES
+ENOLASE
+LOANEES
+AREOLES
+AUREOLE
+OLEATES
+AENEOUS
+ELOINED
+REOILED
+OREIDES
+OSIERED
+OLEINES
+LOERIES
+ESTOILE
+ETOILES
+ERINITE
+NITERIE
+ORATING
+ROATING
+ISOBARE
+BOATIES
+CODEINA
+OCEANID
+CALORIE
+CARIOLE
+COALIER
+LORICAE
+ALOETIC
+ACINOSE
+CARIOSE
+ORACIES
+SCORIAE
+SOCIATE
+AMIDONE
+DOMAINE
+NAEVOID
+OPIATED
+AVODIRE
+AVOIDER
+FOLIATE
+AIRHOLE
+MINEOLA
+LOAMIER
+OPALINE
+PELORIA
+VARIOLE
+VIOLATE
+ANOMIES
+MOINEAU
+AIRSOME
+AMOSITE
+ATOMIES
+ATOMISE
+OSMIATE
+EPINAOS
+SENOPIA
+EVASION
+SOAPIER
+ATOPIES
+OPIATES
+OVARIES
+ERODENT
+ESTRONE
+TAENITE
+TENIATE
+TEARIER
+ARIETTE
+ITERATE
+BARONET
+REBOANT
+ENACTOR
+ANOTHER
+TONEARM
+OPERANT
+PRONATE
+PROTEAN
+VENATOR
+BORNITE
+COINTER
+NOTICER
+RECTION
+THRENOI
+POINTER
+PROTEIN
+PTERION
+REPOINT
+TROPINE
+NOWTIER
+TOWNIER
+ODONATE
+ODORATE
+LOONIER
+TROOLIE
+EROSION
+ISOTONE
+TOONIES
+OORIEST
+ROOTIES
+SOOTIER
+TOORIES
+AEONIAN
+ANERGIA
+BEANIER
+BETAINE
+BEATIER
+EBRIATE
+CINEREA
+HERNIAE
+REMANIE
+ETAMINE
+MATINEE
+EMERITA
+EMIRATE
+MEATIER
+PERINEA
+NAIVETE
+PEATIER
+EVIRATE
+GEOIDAL
+GOALIES
+SOILAGE
+EULOGIA
+AREOLAE
+GROANED
+TANGOED
+GAROTED
+TANGELO
+GLOATER
+LEGATOR
+ONAGERS
+ORANGES
+ONSTAGE
+TANGOES
+GAROTES
+ORGEATS
+STORAGE
+TOERAGS
+OUTRAGE
+ERODING
+GROINED
+IGNORED
+REDOING
+INGOTED
+GOITRED
+LENTIGO
+ERINGOS
+IGNORES
+REGIONS
+SIGNORE
+GOITERS
+GOITRES
+GORIEST
+GOUTIER
+AEROBIA
+EPINAOI
+DENIALS
+SNAILED
+ALIUNDE
+UNIDEAL
+DERAILS
+DIALERS
+REDIALS
+SIDERAL
+UREDIAL
+DETAILS
+DILATES
+RESIDUA
+DAUTIES
+INSULAE
+INULASE
+SITULAE
+DARNEST
+ENDARTS
+STANDER
+STARNED
+DAUNTER
+NATURED
+UNRATED
+UNTREAD
+ANTLERS
+RENTALS
+SALTERN
+SLANTER
+STERNAL
+NEUTRAL
+AUNTERS
+NATURES
+SAUNTER
+TENDRIL
+TRINDLE
+SNIRTED
+TINDERS
+INTRUDE
+TURDINE
+UNTIRED
+UNTRIDE
+UNTRIED
+LINTERS
+SLINTER
+SNIRTLE
+NUTSIER
+TRIUNES
+UNITERS
+LINEAGE
+EGALITE
+AGENISE
+EUGENIA
+EUGARIE
+ORDINAL
+DILATOR
+DONAIRS
+INROADS
+ORDAINS
+SADIRON
+ASTROID
+AUDITOR
+LATINOS
+TALIONS
+OUTLAIN
+ORALIST
+RIALTOS
+SLIOTAR
+TAILORS
+SAUTOIR
+NEONATE
+ONERIER
+GRANTEE
+GREATEN
+NEGATER
+REAGENT
+GENTIER
+INTEGER
+TEERING
+TREEING
+ABORTEE
+CORNEAE
+ACETONE
+OCREATE
+EROTEMA
+PERAEON
+OPERATE
+OVERATE
+OVEREAT
+EBONITE
+COTERIE
+HEROINE
+ONETIME
+PEREION
+PERONEI
+PIONEER
+RADIALE
+NAIADES
+ARAISED
+AERIALS
+AURELIA
+DELIRIA
+IRIDEAL
+AIRSIDE
+DAIRIES
+DIARIES
+DIARISE
+LAIRISE
+LAITIES
+ALIENEE
+ANESTRA
+SANTERA
+NATURAE
+TAUREAN
+INDITER
+NITRIDE
+LINTIER
+NITRILE
+GANOINE
+TEENIER
+LOADENS
+LOADERS
+ORDEALS
+RELOADS
+ROULADE
+SALTOED
+SOLATED
+OUTLEAD
+DOUANES
+AROUSED
+DOLINES
+INDOLES
+SONDELI
+UNOILED
+SOLDIER
+SOLIDER
+OUTLIED
+TOLUIDE
+OUTSIDE
+TEDIOUS
+ELUSION
+LOURIES
+LOUSIER
+SOILURE
+OUTLIES
+AILANTO
+ALATION
+ATONIAS
+ENTROLD
+RODENTS
+SNORTED
+LENTORS
+LORNEST
+TENOURS
+TONSURE
+DOGEATE
+GOATEED
+AEROGEL
+GOATEES
+GENOISE
+SOIGNEE
+EGOTISE
+GOETIES
+NARDINE
+DRAINER
+RANDIER
+TAINTED
+TARDIER
+TARRIED
+ATTIRED
+LARNIER
+RETIRAL
+RETRIAL
+TRAILER
+TERTIAL
+INSANER
+INSNARE
+ANEURIN
+INANEST
+NANITES
+STANINE
+TANNIES
+SIERRAN
+SNARIER
+INSTATE
+SATINET
+TANTIES
+ARTSIER
+SERRATI
+TARRIES
+TARSIER
+ARTIEST
+ARTISTE
+ATTIRES
+IRATEST
+RATITES
+STRIATE
+TASTIER
+TERTIAS
+AEDILES
+DEISEAL
+BEGONIA
+COINAGE
+LEARNED
+EDENTAL
+LATENED
+ALERTED
+ALTERED
+REDEALT
+RELATED
+TREADLE
+DEANERS
+ENDEARS
+UNEARED
+STANDEE
+STEANED
+DEAREST
+DERATES
+ESTRADE
+REASTED
+REDATES
+SEDATER
+STEARED
+TASERED
+LEANERS
+ELANETS
+LATEENS
+LEANEST
+ELATERS
+REALEST
+RELATES
+RESLATE
+STEALER
+AUSTERE
+REDLINE
+RELINED
+LENITED
+RETILED
+DENIERS
+DERNIES
+NEREIDS
+RESINED
+UREDINE
+DESTINE
+ENDITES
+STEINED
+DETINUE
+DIESTER
+DIETERS
+REEDITS
+REISTED
+RESITED
+ERUDITE
+LIERNES
+RELINES
+LENITES
+LISENTE
+SETLINE
+TENSILE
+LEISTER
+RETILES
+STERILE
+ENSUITE
+SUETIER
+BANDIER
+BRAINED
+REDBAIT
+TRIBADE
+LIBRATE
+TABLIER
+TRIABLE
+BANTIES
+BASINET
+BESAINT
+BESTAIN
+BAITERS
+BARITES
+REBAITS
+TERBIAS
+CAIRNED
+CARNIED
+DANCIER
+CARLINE
+ARTICLE
+RECITAL
+TALCIER
+ARCSINE
+ARSENIC
+CARNIES
+CERASIN
+ACETINS
+CANIEST
+CINEAST
+TUNICAE
+ATRESIC
+CRISTAE
+RACIEST
+STEARIC
+FRIANDE
+DEFIANT
+FAINTED
+HANDIER
+AIRTHED
+ADERMIN
+INARMED
+MEDIANT
+READMIT
+PARDINE
+DEPAINT
+PAINTED
+PATINED
+INVADER
+RAVINED
+DEVIANT
+DIPTERA
+PARTIED
+PIRATED
+TARDIVE
+DIETARY
+INFLATE
+INFARES
+SERAFIN
+FAINEST
+FANSITE
+NAIFEST
+FAIREST
+HERNIAL
+INHALER
+LATHIER
+ARSHINE
+HERNIAS
+NEARISH
+HENTAIS
+SHEITAN
+STHENIA
+HASTIER
+SHERIAT
+MANLIER
+MARLINE
+MINERAL
+RAILMEN
+AILMENT
+ALIMENT
+LAMITER
+MALTIER
+MARLITE
+PEARLIN
+PLAINER
+PRALINE
+PANTILE
+RAVELIN
+LAWNIER
+INLAYER
+NAILERY
+VENTAIL
+PLAITER
+PLATIER
+WALTIER
+IRATELY
+REALITY
+TEARILY
+MARINES
+REMAINS
+SEMINAR
+SIRNAME
+ETAMINS
+INMATES
+MAINEST
+MANTIES
+TAMEINS
+TAMINES
+IMARETS
+MAESTRI
+MAISTER
+MASTIER
+MISRATE
+SEMITAR
+SMARTIE
+MURIATE
+PANIERS
+RAPINES
+PANTIES
+PATINES
+SAPIENT
+SPINATE
+PETUNIA
+AVENIRS
+RAVINES
+VAURIEN
+UNWARIE
+NAIVEST
+NATIVES
+VAINEST
+AWNIEST
+TAWNIES
+WANIEST
+WANTIES
+VAUNTIE
+PAIREST
+PARTIES
+PASTIER
+PIASTER
+PIASTRE
+PIRATES
+PRATIES
+RAPIEST
+TRAIPSE
+RAVIEST
+TAIVERS
+VASTIER
+VERITAS
+WAISTER
+WAITERS
+WARIEST
+WASTRIE
+AUREITY
+OREGANO
+ROOTAGE
+GOONIER
+AREOLAS
+AUREOLA
+IODINES
+IONISED
+IODISER
+ELISION
+ISOLINE
+LIONISE
+NOILIES
+SOILIER
+IOLITES
+OILIEST
+TABORIN
+CAROTIN
+CORTINA
+ORTHIAN
+TORMINA
+ATROPIN
+WAITRON
+TARAIRE
+ARIETTA
+RAINIER
+ALIGNED
+DEALIGN
+DEALING
+LEADING
+GLADIER
+GLAIRED
+LIGATED
+TAIGLED
+AGNISED
+AGRISED
+GAUDIER
+AGISTED
+LEASING
+LINAGES
+SEALING
+LINGUAE
+UNAGILE
+GLAIRES
+GRAILES
+AGILEST
+AIGLETS
+GELATIS
+LIGATES
+TAIGLES
+GLUTAEI
+GUINEAS
+AUGITES`.split('\n').map(word => word.trim().toUpperCase()).filter(word => word.length > 0);
+
+        let currentWord = '';
+        let shuffledWord = '';
+        let possibleWords = [];
+        let guessedWords = new Set();
+
+        const shuffledWordDisplay = document.getElementById('shuffled-word');
+        const userInput = document.getElementById('user-input');
+        const submitButton = document.getElementById('submit-button');
+        const nextWordButton = document.getElementById('next-word-button');
+        const messageBox = document.getElementById('message-box');
+        const correctGuessesList = document.getElementById('correct-guesses-list');
+        const guessedWordsUl = document.getElementById('guessed-words-ul');
+
+        /**
+         * Shuffles the letters of a given word.
+         * @param {string} word The word to shuffle.
+         * @returns {string} The shuffled word.
+         */
+        function shuffleWord(word) {
+            const a = word.split('');
+            for (let i = a.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [a[i], a[j]] = [a[j], a[i]];
+            }
+            return a.join('');
+        }
+
+        /**
+         * Generates all permutations of a given string.
+         * This function is used to find all possible words from an anagram.
+         * @param {string} str The string to permute.
+         * @returns {Set<string>} A set of all unique permutations.
+         */
+        function getPermutations(str) {
+            const results = new Set();
+
+            /**
+             * Recursive helper function to generate permutations.
+             * @param {string} currentPermutation The current permutation being built.
+             * @param {string} remainingChars The characters remaining to be used.
+             */
+            function permute(currentPermutation, remainingChars) {
+                if (remainingChars.length === 0) {
+                    results.add(currentPermutation);
+                    return;
+                }
+
+                for (let i = 0; i < remainingChars.length; i++) {
+                    const char = remainingChars[i];
+                    const newRemainingChars = remainingChars.slice(0, i) + remainingChars.slice(i + 1);
+                    permute(currentPermutation + char, newRemainingChars);
+                }
+            }
+
+            permute('', str);
+            return results;
+        }
+
+        /**
+         * Finds all valid words that can be formed from the letters of a given word.
+         * @param {string} letters The letters to form words from.
+         * @param {Array<string>} dictionary The list of valid words.
+         * @returns {Array<string>} An array of valid words.
+         */
+        function findPossibleWords(letters, dictionary) {
+            const possible = new Set();
+            const sortedLetters = letters.split('').sort().join(''); // Sort for canonical representation
+
+            // Iterate through the dictionary to find words that will work
+            for (const word of dictionary) {
+                // Only consider words of the same length for direct anagrams
+                if (word.length === letters.length) {
+                    const sortedWord = word.split('').sort().join('');
+                    if (sortedWord === sortedLetters) {
+                        possible.add(word);
+                    }
+                }
+            }
+            return Array.from(possible);
+        }
+
+        /**
+         * Displays a message to the user.
+         * @param {string} message The message to display.
+         * @param {string} type The type of message (e.g., 'success', 'error', 'info').
+         */
+        function showMessage(message, type) {
+            messageBox.textContent = message;
+            messageBox.style.display = 'block';
+            messageBox.className = 'message-box'; // Reset classes
+
+            if (type === 'success') {
+                messageBox.classList.add('bg-green-100', 'text-green-800', 'border-green-400');
+            } else if (type === 'error') {
+                messageBox.classList.add('bg-red-100', 'text-red-800', 'border-red-400');
+            } else { // info
+                messageBox.classList.add('bg-blue-100', 'text-blue-800', 'border-blue-400');
+            }
+        }
+
+        /**
+         * Hides the message box.
+         */
+        function hideMessage() {
+            messageBox.style.display = 'none';
+        }
+
+        /**
+         * Updates the list of correctly guessed words.
+         */
+        function updateGuessedWordsList() {
+            guessedWordsUl.innerHTML = ''; // Clear previous list
+            if (guessedWords.size > 0) {
+                correctGuessesList.classList.remove('hidden');
+                guessedWords.forEach(word => {
+                    const listItem = document.createElement('li');
+                    listItem.textContent = word;
+                    guessedWordsUl.appendChild(listItem);
+                });
+            } else {
+                correctGuessesList.classList.add('hidden');
+            }
+        }
+
+        /**
+         * Starts a new round of the game.
+         */
+        function newRound() {
+            hideMessage();
+            userInput.value = '';
+            guessedWords.clear();
+            updateGuessedWordsList();
+            nextWordButton.classList.add('hidden');
+            submitButton.classList.remove('hidden');
+            userInput.disabled = false;
+
+            // Choose a random word from the list
+            currentWord = wordList[Math.floor(Math.random() * wordList.length)];
+
+            // Find all possible words for the current anagram
+            possibleWords = findPossibleWords(currentWord, wordList);
+
+            // Shuffle the current word for display
+            shuffledWord = shuffleWord(currentWord);
+            // Ensure the shuffled word is different from the original if there's only one possible word
+            while (shuffledWord === currentWord && possibleWords.length === 1) {
+                shuffledWord = shuffleWord(currentWord);
+            }
+            shuffledWordDisplay.textContent = shuffledWord;
+            userInput.focus();
+        }
+
+        /**
+         * Handles the user's guess submission.
+         */
+        function handleSubmit() {
+            const guess = userInput.value.trim().toUpperCase();
+            userInput.value = ''; // Clear input after guess
+
+            if (guess === '') {
+                showMessage('Please enter a word!', 'info');
+                return;
+            }
+
+            // Check if the guess is one of the possible words for the current anagram
+            if (possibleWords.includes(guess)) {
+                if (!guessedWords.has(guess)) {
+                    guessedWords.add(guess);
+                    showMessage(`Correct! You guessed "${guess}".`, 'success');
+                    updateGuessedWordsList();
+
+                    // Check if all possible words have been guessed
+                    if (guessedWords.size === possibleWords.length) {
+                        showMessage(`Excellent! You found all possible words for "${shuffledWord}".`, 'success');
+                        messageBox.classList.add('correct-message');
+                        submitButton.classList.add('hidden');
+                        nextWordButton.classList.remove('hidden');
+                        userInput.disabled = true;
+                    }
+                } else {
+                    showMessage(`You already guessed "${guess}". Try another one!`, 'info');
+                }
+            } else {
+                showMessage(`Incorrect. "${guess}" is not a valid word for this anagram.`, 'error');
+            }
+            userInput.focus(); // Keep focus on input for next guess
+        }
+
+        // Event Listeners
+        submitButton.addEventListener('click', handleSubmit);
+        nextWordButton.addEventListener('click', newRound);
+
+        // Modify the keypress listener to handle both submit and next word
+        document.addEventListener('keypress', (event) => {
+            if (event.key === 'Enter') {
+                if (!submitButton.classList.contains('hidden')) { // If submit button is visible
+                    handleSubmit();
+                } else if (!nextWordButton.classList.contains('hidden')) { // If next word button is visible
+                    newRound();
+                }
+            }
+        });
+
+        // Initialize the game when the window loads
+        window.onload = newRound;
